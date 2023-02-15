fd65d4f390268f333ca33ea70c4c4e67724d3091
Osiozekhai Aliu <aliu@dev-hh.de>
2023-02-15 11:02:41 +0100

    WIP

--- Plugins/Catalog/Model/ProductPlugin.php
+++ Plugins/Catalog/Model/ProductPlugin.php
@@ -3,46 +3,59 @@
 namespace Osio\Subscriptions\Plugins\Catalog\Model;
 
 use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\Product\OptionFactory;
+use Magento\Catalog\Model\Product\Option;
 
 class ProductPlugin
 {
-    private OptionFactory $optionFactory;
 
-    public function __construct(OptionFactory $optionFactory)
+    private Option $option;
+
+    public function __construct(Option $option)
     {
-        $this->optionFactory = $optionFactory;
+        $this->option = $option;
     }
 
-    private function getOption()
+    public function beforeSave(Product $product): array
     {
-        return $this->optionFactory->create();
-    }
-
-    public function beforeSave(Product $product): void
-    {
-        $this->getOption()->addData($this->getCustomOptions($product));
-        $product->addOption($this->getOption());
+        $this->option->addData($this->getCustomOptions($product));
+        $product->addOption($this->option);
         $product->setData('has_options', true);
+
+        return [];
     }
 
     private function getCustomOptions(Product $product): array
     {
         return [
-            "sort_order" => 1,
-            "title" => "Custom Options",
-            "price_type" => "fixed",
-            "price" => "",
-            "type" => "drop_down",
-            "is_require" => false,
-            "product_id" => $product->getData('id'),
-            "sku" => $product->getData('sku'),
-            "store_id" => $product->getData('store_id'),
-            "values" => [
-                ["title" => "Option 1", "price" => 10, "price_type" => "fixed", "sort_order" => 1],
-                ["title" => "Option 2", "price" => 20, "price_type" => "fixed", "sort_order" => 2],
-                ["title" => "Option 3", "price" => 30, "price_type" => "fixed", "sort_order" => 30]
-            ]
+            'sort_order' => 1,
+            'title'      => 'Custom Options',
+            'price_type' => 'fixed',
+            'price'      => '',
+            'type'       => 'drop_down',
+            'is_require' => false,
+            'product_id' => $product->getData('id'),
+            'sku'        => $product->getData('sku'),
+            'store_id'   => $product->getData('store_id'),
+            'values'     => [
+                [
+                    'title'      => 'Option 1',
+                    'price'      => 10,
+                    'price_type' => 'fixed',
+                    'sort_order' => 1,
+                ],
+                [
+                    'title'      => 'Option 2',
+                    'price'      => 20,
+                    'price_type' => 'fixed',
+                    'sort_order' => 2,
+                ],
+                [
+                    'title'      => 'Option 3',
+                    'price'      => 30,
+                    'price_type' => 'fixed',
+                    'sort_order' => 30,
+                ],
+            ],
         ];
     }
 
