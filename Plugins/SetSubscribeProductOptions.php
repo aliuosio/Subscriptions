ba987a3ed0f759b2ed8ff86d28f5c0fcca1e29ae
Osiozekhai Aliu <aliu@dev-hh.de>
2023-03-28 01:21:07 +0200

    property promotion and refactoring

--- Plugins/SetSubscribeProductOptions.php
+++ Plugins/SetSubscribeProductOptions.php
@@ -8,18 +8,19 @@ namespace Osio\Subscriptions\Plugins;
 
 use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class SetSubscribeProductOptions
 {
-    private ProductCustomOptionInterface $option;
 
-    /**
-     * @param ProductCustomOptionInterface $option
-     */
-    public function __construct(ProductCustomOptionInterface $option)
-    {
-        $this->option = $option;
-    }
+
+    const TITLE = 'system/subscribable/title';
+    const PERIODS = 'system/subscribable/periods';
+
+    public function __construct(
+        private readonly ProductCustomOptionInterface $option,
+        private readonly ScopeConfigInterface $scopeConfig
+    ) {}
 
     /**
      * @param ProductInterface $product
@@ -27,22 +28,61 @@ class SetSubscribeProductOptions
      */
     public function beforeSave(ProductInterface $product): array
     {
-        $this->option->addData($this->getCustomOptions($product));
-        $product->addOption($this->option);
-        $product->setData('has_options', true);
+        $this->option->addData(
+            $this->getCustomOptions($product, $this->getTitle(), $this->getValues())
+        );
+        $product->addOption($this->option)
+            ->setData('has_options', true);
 
         return [];
     }
 
     /**
-     * @param ProductInterface $product
-     * @return array{sort_order: integer}
+     * @return array
      */
-    private function getCustomOptions(ProductInterface $product): array
+    private function getValues(): array
+    {
+        return [
+            [
+                'title' => 'Option 1',
+                'price' => 10,
+                'price_type' => 'fixed',
+                'sort_order' => 1,
+            ],
+            [
+                'title' => 'Option 2',
+                'price' => 20,
+                'price_type' => 'fixed',
+                'sort_order' => 2,
+            ],
+            [
+                'title' => 'Option 3',
+                'price' => 30,
+                'price_type' => 'fixed',
+                'sort_order' => 30,
+            ],
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getTitle(): mixed
+    {
+        return $this->scopeConfig->getValue(self::TITLE);
+    }
+
+    /**
+     * @param ProductInterface $product
+     * @param mixed $title
+     * @param array $values
+     * @return array
+     */
+    private function getCustomOptions(ProductInterface $product, mixed $title, array $values): array
     {
         return [
             'sort_order' => 1,
-            'title' => 'Custom Options',
+            'title' => $title,
             'price_type' => 'fixed',
             'price' => '',
             'type' => 'drop_down',
@@ -50,26 +90,15 @@ class SetSubscribeProductOptions
             'product_id' => $product->getId(),
             'sku' => $product->getSku(),
             'store_id' => $product->getData('store_id'),
-            'values' => [
-                [
-                    'title' => 'Option 1',
-                    'price' => 10,
-                    'price_type' => 'fixed',
-                    'sort_order' => 1,
-                ],
-                [
-                    'title' => 'Option 2',
-                    'price' => 20,
-                    'price_type' => 'fixed',
-                    'sort_order' => 2,
-                ],
-                [
-                    'title' => 'Option 3',
-                    'price' => 30,
-                    'price_type' => 'fixed',
-                    'sort_order' => 30,
-                ],
-            ],
+            'values' => $values,
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCustomOptionValues(): array
+    {
+        return explode(',', $this->scopeConfig->getValue(self::PERIODS));
     }
 }
