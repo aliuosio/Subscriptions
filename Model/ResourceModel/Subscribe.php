19abf2e75374c8e0716b3438cde61f6dd64c02d1
Osiozekhai Aliu <oaliu@hotmail.com>
2023-04-22 22:58:00 +0200

    revert

---
+++ Model/ResourceModel/Subscribe.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Osio\Subscriptions\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Framework\Model\ResourceModel\Db\Context;
+
+class Subscribe extends AbstractDb
+{
+    public function __construct(
+        Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+    protected function _construct()
+    {
+        $this->_init('subscriptions', 'id');
+    }
+}
