14a42da98a44c8ab3e01c3f71ce5c6307d9af4c2
Osiozekhai Aliu <oaliu@hotmail.com>
2023-04-22 13:21:59 +0200

    refactored

--- Plugins/SetSubscribeProductOptions.php
+++ Plugins/SetSubscribeProductOptions.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace Osio\Subscriptions\Plugins;
 
 use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
+use Magento\Catalog\Api\Data\ProductCustomOptionInterfaceFactory;
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Model\Product\Option;
 use Osio\Subscriptions\Helper\Data as Helper;
 
 class SetSubscribeProductOptions
 {
     public function __construct(
-        private readonly ProductCustomOptionInterface $option,
-        private readonly Helper                       $helper
+        private readonly productCustomOptionInterfaceFactory $optionInterfaceFactory,
+        private readonly Helper                              $helper
     )
     {
     }
@@ -24,12 +24,12 @@ class SetSubscribeProductOptions
             return false;
         }
 
-        if ($this->isOptionFlagSet($product) && !$this->hasThisOption($product->getOptions())) {
+        if ($this->isOptionFlagSet($product) && !$this->hasThisOption($product)) {
             $this->addOption($product);
         }
 
-        if (!$this->isOptionFlagSet($product) && $this->hasThisOption($product->getOptions())) {
-            $this->removeOption($product);
+        if (!$this->isOptionFlagSet($product) && $this->hasThisOption($product)) {
+            $this->resetOptions($product);
         }
 
         return false;
@@ -37,62 +37,58 @@ class SetSubscribeProductOptions
 
     private function isOptionFlagSet(ProductInterface $product): bool
     {
-        return (bool)$product->getData(e);
+        return (bool)$product->getData($this->helper->getCode());
     }
 
-    private function hasThisOption($options): bool
+    private function hasThisOption(ProductInterface $product): bool
     {
-        /** @var Option $option */
-        foreach ($options as $option) {
-            if ($option->getTitle() == $this->helper->getTitle()) {
-                return true;
-            }
-        }
+        $titles = array_map(function ($option) {
+            return $option->getTitle();
+        }, $product->getOptions());
 
-        return false;
+        return in_array($this->helper->getTitle(), $titles, true);
     }
 
-    private function removeOption($options): void
+    private function resetOptions(ProductInterface $product): void
     {
-        /** @var Option $option */
-        foreach ($options as $option) {
-            if ($option->getTitle() == $this->helper->getTitle()) {
-                $option->delete();
-                break;
-            }
-        }
+        $optionsReset = array_filter($product->getOptions(), function ($option) {
+            return $option->getTitle() !== $this->helper->getTitle();
+        });
+
+        $product->setOptions(array_values($optionsReset));
+    }
+
+    private function getOption(): ProductCustomOptionInterface
+    {
+        return $this->optionInterfaceFactory->create();
     }
 
     private function addOption(ProductInterface $product): void
     {
-        $this->option->addData(
-            $this->getCustomOptions($product, $this->helper->getTitle(), $this->getValues())
+        $this->getOption()->setData(
+            $this->getCustomOptions(
+                $product,
+                $this->helper->getTitle(),
+                $this->getValues($this->helper->getPeriod())
+            )
         );
-        $product->addOption($this->option)->setData('has_options', true);
+        $product->addOption($this->getOption())->setData('has_options', true);
     }
 
-    private function getValues(): array
+    private function getValues(array $titles): array
     {
-        return [
-            [
-                'title' => 'Option 1',
-                'price' => 10,
+        $options = [];
+
+        foreach ($titles as $index => $title) {
+            $options[] = [
+                'title' => $title,
+                'price' => 0,
                 'price_type' => 'fixed',
-                'sort_order' => 1,
-            ],
-            [
-                'title' => 'Option 2',
-                'price' => 20,
-                'price_type' => 'fixed',
-                'sort_order' => 2,
-            ],
-            [
-                'title' => 'Option 3',
-                'price' => 30,
-                'price_type' => 'fixed',
-                'sort_order' => 30,
-            ],
-        ];
+                'sort_order' => $index + 1,
+            ];
+        }
+
+        return $options;
     }
 
     private function getCustomOptions(ProductInterface $product, mixed $title, array $values): array
