d9e096ca786493ce911e7bec90748810e388c717
Osiozekhai Aliu <oaliu@hotmail.com>
2023-04-24 10:08:57 +0200

    save items from completed order to subscription table if product option subscription was selected

--- Plugins/OrderItemsToSubscribeSet.php
+++ Plugins/OrderItemsToSubscribeSet.php
@@ -4,23 +4,50 @@ declare(strict_types=1);
 
 namespace Osio\Subscriptions\Plugins;
 
+use Magento\Framework\App\ResourceConnection;
 use Magento\Sales\Api\Data\OrderInterface;
 use Osio\Subscriptions\Helper\Data as Helper;
 
 class OrderItemsToSubscribeSet
 {
     public function __construct(
-        private readonly Helper $helper
-    ) {
+        private readonly Helper             $helper,
+        private readonly ResourceConnection $resource
+    )
+    {
     }
 
-    public function afterSetStatus(OrderInterface $subject, OrderInterface $result)
+    public function afterSetStatus(OrderInterface $subject, OrderInterface $result): OrderInterface
     {
+        if ($this->helper->isEnabled() && $result->getStatus() == 'complete') {
 
-        if ($this->helper->isEnabled()) {
-            $result->getStatus();
+            $data = [];
+
+            foreach ($result->getItems() as $item) {
+                foreach ($item->getData('product_options') as $options) {
+                    if (is_array($options)) {
+                        foreach ($options as $option) {
+                            if (isset($option['label']) && $option['label'] == $this->helper->getTitle()) {
+                                $data[$item->getItemId()]['item_id'] = $item->getItemId();
+                                $data[$item->getItemId()]['customer_id'] = $result->getCustomerId();
+                                $data[$item->getItemId()]['period'] = $option['value'];
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!empty($data)) {
+                $this->insertMultiple($data);
+            }
         }
 
         return $result;
+    }
+
+    public function insertMultiple(array $data): int
+    {
+        return $this->resource->getConnection()
+            ->insertMultiple('subscriptions', $data);
     }
 }
