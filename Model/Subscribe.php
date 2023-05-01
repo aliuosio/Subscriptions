88282a0b62425b2a982b3f81a7b8d1334ed56146
Osiozekhai Aliu <oaliu@hotmail.com>
2023-05-01 12:00:28 +0200

    revert

---
+++ Model/Subscribe.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Osio\Subscriptions\Model;
+
+use Magento\Framework\Model\AbstractModel;
+use Osio\Subscriptions\Model\ResourceModel\Subscribe as ResourceModelSubscribe;
+use Magento\Framework\DataObject\IdentityInterface;
+
+class Subscribe extends AbstractModel implements IdentityInterface
+{
+    public const CACHE_TAG = 'osio_subscriptions_subscribe';
+    protected string $cacheTag = 'osio_subscriptions_subscribe';
+    protected string $eventPrefix = 'osio_subscriptions_subscribe';
+
+    protected function _construct()
+    {
+        $this->_init(ResourceModelSubscribe::class);
+    }
+
+    public function getIdentities(): array
+    {
+        return [Subscribe::CACHE_TAG . '_' . $this->getId()];
+    }
+
+    public function getDefaultValues(): array
+    {
+        return [];
+    }
+}
