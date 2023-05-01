116eec128c951e78ce588d3cd5fd6c6613ec32ed
Osiozekhai Aliu <oaliu@hotmail.com>
2023-05-01 22:46:03 +0200

    added cronjob

---
+++ Cron/Run.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Osio\Subscriptions\Cron;
+
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Osio\Subscriptions\Model\ReOrder;
+
+class Run
+{
+    public function __construct(
+        private readonly ReOrder $reOrder
+    )
+    {
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     * @throws InputException
+     */
+    protected function execute(): void
+    {
+        $this->reOrder->execute();
+    }
+}
