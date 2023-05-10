276429b269e4b3532d1efb460990b15636aec30c
Osiozekhai Aliu <oaliu@hotmail.com>
2023-05-10 17:36:22 +0200

    added customer address data

--- Model/ReOrder/Customers.php
+++ Model/ReOrder/Customers.php
@@ -7,22 +7,42 @@ namespace Osio\Subscriptions\Model\ReOrder;
 use Magento\Customer\Model\ResourceModel\Customer\Collection as customerCollection;
 use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as customerCollectionFactory;
 use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Exception\LocalizedException;
 
 class Customers
 {
 
     public function __construct(
-        private readonly customerCollectionFactory $customerCollectionFactory
-    ) {
+        private readonly customerCollectionFactory $customerCollectionFactory,
+    )
+    {
     }
 
+    /**
+     * @throws LocalizedException
+     */
     private function getCustomerCollection(): customerCollection
     {
-        return $this->customerCollectionFactory->create();
+        $collection = $this->customerCollectionFactory->create();
+        $collection->getSelect()->join(
+            ['address' => 'customer_address_entity'],
+            'e.entity_id = address.parent_id',
+            ['address.*']
+        );
+        $collection->addFieldToSelect('entity_id');
+
+        return $collection;
     }
 
+
+    /**
+     * @throws LocalizedException
+     */
     public function fetchCustomers(array $customerIds): AbstractDb|customerCollection
     {
-        return $this->getCustomerCollection()->addFieldToFilter('entity_id', ['in' => $customerIds]);
+        $collection = $this->getCustomerCollection();
+        $collection->addFieldToFilter('e.entity_id', ['in' => $customerIds]);
+
+        return $collection;
     }
 }
